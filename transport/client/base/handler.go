71858788ed8bca36cf70719765438e120f899d28
adranwit <adrianwit@gmail.com>
2025-04-13 07:13:24 -0700

    updated implementation

--- transport/client/base/handler.go
+++ transport/client/base/handler.go
@@ -8,18 +8,18 @@ import (
 // Handler represents a default handler
 type Handler struct{}
 
-func (h *Handler) Serve(ctx context.Context, request *jsonrpc.Request, response *jsonrpc.Response) *jsonrpc.Error {
+func (h *Handler) Serve(_ context.Context, request *jsonrpc.Request, response *jsonrpc.Response) *jsonrpc.Error {
 	response.Id = request.Id
 	response.Jsonrpc = request.Jsonrpc
 	return jsonrpc.NewMethodNotFound(request.Id, request.Method, nil)
 }
 
-func (h *Handler) OnNotification(ctx context.Context, notification *jsonrpc.Notification) *jsonrpc.Error {
+func (h *Handler) OnNotification(_ context.Context, _ *jsonrpc.Notification) *jsonrpc.Error {
 	//ignore
 	return nil
 }
 
-func (h *Handler) OnError(ctx context.Context, error *jsonrpc.Error) *jsonrpc.Error {
+func (h *Handler) OnError(_ context.Context, _ *jsonrpc.Error) *jsonrpc.Error {
 	//ignore
 	return nil
 }
